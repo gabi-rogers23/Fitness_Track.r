f6d931b52b2b0d2f89520cdb30bf2e414e408d2b
Gabrielle Rogers <gabirogers23@gmail.com>
2023-01-30 21:22:08 -0600

    WIP Tables

--- db/users.js
+++ db/users.js
@@ -4,24 +4,35 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+     INSERT INTO users(username, password)
+     VALUES($1, $2)
+     RETURNING *;
+    `,
+      [username, password]
+    );
+
+    user.password = null;
+console.log("CREATE USER FUNCTION RETURNING: ", user)
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function getUser({ username, password }) {
+async function getUser({ username, password }) {}
 
-}
+async function getUserById(userId) {}
 
-async function getUserById(userId) {
-
-}
-
-async function getUserByUsername(userName) {
-
-}
+async function getUserByUsername(userName) {}
 
 module.exports = {
   createUser,
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
