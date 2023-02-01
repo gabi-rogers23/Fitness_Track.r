3be0d39a4dc7ab8957afeb40a7d1d366fd41f7eb
Alexandra Labaisse <beatsnmelody1@gmail.com>
2023-01-31 20:57:22 -0600

    started createActivity

--- db/activities.js
+++ db/activities.js
@@ -2,7 +2,24 @@ const client = require('./client');
 
 // database functions
 async function createActivity({ name, description }) {
-  // return the new activity
+  try {
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
+     INSERT INTO activities(name, description)
+     VALUES($1, $2)
+     RETURNING *;
+    `,
+      [name, description]
+    );
+
+    name.description = null;
+console.log("CREATE ACTIVITY FUNCTION RETURNING: ", activity)
+    return activity;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getAllActivities() {
