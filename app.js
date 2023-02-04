55bfaafcc3b09c9a4ff27f89cf90c0045b7a7d1f
Gabrielle Rogers <gabirogers23@gmail.com>
2023-02-04 00:51:12 -0600

    setup server app

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ const express = require("express")
 const app = express()
 
 // Setup your Middleware and API Router here
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+const apiRouter = require("./api");
+app.use("/api", apiRouter);
 
 module.exports = app;
