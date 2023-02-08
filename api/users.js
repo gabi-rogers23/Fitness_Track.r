3bb56964852251b51023fc5126fecc59472dadda
Gabrielle Rogers <gabirogers23@gmail.com>
2023-02-08 15:09:56 -0600

    ape/users/login passing tests

--- api/users.js
+++ api/users.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
 const router = express.Router();
-const { getUserByUsername, createUser } = require("../db");
-const jwt = require('jsonwebtoken');
+const { getUserByUsername, createUser, getUser } = require("../db");
+const jwt = require("jsonwebtoken");
 
 // POST /api/users/register
 router.post("/register", async (req, res, next) => {
@@ -26,9 +26,9 @@ router.post("/register", async (req, res, next) => {
     } else {
       const user = await createUser({
         username,
-        password
+        password,
       });
-  
+
       const token = jwt.sign(
         {
           id: user.id,
@@ -39,13 +39,13 @@ router.post("/register", async (req, res, next) => {
           expiresIn: "1w",
         }
       );
-  
-      console.log("USER BEING RETURNED FROM DB IN API: ", await user);
-  
+
+      // console.log("USER BEING RETURNED FROM DB IN API: ", await user);
+
       res.send({
         message: "thank you for signing up",
         token: token,
-        user: user
+        user: user,
       });
     }
   } catch (error) {
@@ -54,7 +54,39 @@ router.post("/register", async (req, res, next) => {
 });
 
 // POST /api/users/login
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
 
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+
+    const user = await getUser(req.body);
+console.log(user)
+
+    if (!user) {
+     next({
+      name : "UserNotFound", 
+     message: "Username or Password is incorrect!"
+    });
+    } else {
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+
+      res.send({
+        message: "you're logged in!",
+        token: token,
+        user: user,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 // GET /api/users/me
 
 // GET /api/users/:username/routines
