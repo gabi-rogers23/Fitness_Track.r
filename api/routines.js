3f8d59261650fd27c113ff1458626cda25b33c2a
Gabrielle Rogers <gabirogers23@gmail.com>
2023-02-08 21:50:08 -0600

    routines API passing all tests

--- api/routines.js
+++ api/routines.js
@@ -5,6 +5,8 @@ const {
   getRoutineById,
   updateRoutine,
   destroyRoutine,
+  addActivityToRoutine,
+  getRoutineActivitiesByRoutine,
 } = require("../db");
 const router = express.Router();
 const { requireUser } = require("./utils");
@@ -34,13 +36,13 @@ router.post("/", requireUser, async (req, res, next) => {
 router.patch("/:routineId", requireUser, async (req, res, next) => {
   try {
     const userCheck = await getRoutineById(req.params.routineId);
-    
+
     // console.log("ROUTINE USER ID: ",userCheck.creatorId, " | CURRENT USER ID: ",req.user.id);
 
     if (userCheck.creatorId === req.user.id) {
       req.body.id = req.params.routineId;
       const updatedRoutine = await updateRoutine(req.body);
-    //   console.log("UPDATED ROUTINE: ", updatedRoutine);
+      //   console.log("UPDATED ROUTINE: ", updatedRoutine);
       res.send(updatedRoutine);
     } else {
       res.status(403).send({
@@ -56,25 +58,48 @@ router.patch("/:routineId", requireUser, async (req, res, next) => {
 
 // DELETE /api/routines/:routineId
 router.delete("/:routineId", requireUser, async (req, res, next) => {
-    try {
-      const userCheck = await getRoutineById(req.params.routineId);
-      if (userCheck.creatorId === req.user.id) {
-        const deletedRoutine = await destroyRoutine(req.params.routineId);
-        // console.log("DELETED ROUTINE: ", userCheck);
-        res.send(userCheck);
-      } else {
-        res.status(403).send({
-          error: "403 Forbidden",
-          message: `User ${req.user.username} is not allowed to delete ${userCheck.name}`,
-          name: "Forbidden Access",
-        });
-      }
-    } catch (error) {
-      next(error);
+  try {
+    const userCheck = await getRoutineById(req.params.routineId);
+    if (userCheck.creatorId === req.user.id) {
+      const deletedRoutine = await destroyRoutine(req.params.routineId);
+      // console.log("DELETED ROUTINE: ", userCheck);
+      res.send(userCheck);
+    } else {
+      res.status(403).send({
+        error: "403 Forbidden",
+        message: `User ${req.user.username} is not allowed to delete ${userCheck.name}`,
+        name: "Forbidden Access",
+      });
     }
-  });
-
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST /api/routines/:routineId/activities
+router.post("/:routineId/activities", async (req, res, next) => {
+  try {
+    console.log(req.params);
+    const routineActivities = await getRoutineActivitiesByRoutine({
+      id: req.params.routineId,
+    });
+    const attachedActivities = routineActivities.map(
+      (routineActivity) => routineActivity.activityId
+    );
+
+    if (attachedActivities.includes(req.body.activityId)) {
+      res.status(400).send({
+        error: "400 Activity Exists ",
+        message: `Activity ID ${req.body.activityId} already exists in Routine ID ${req.params.routineId}`,
+        name: "This Activity already exists for this Routine",
+      });
+    } else {
+      const addActivity = await addActivityToRoutine(req.body);
+      res.send(addActivity);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
